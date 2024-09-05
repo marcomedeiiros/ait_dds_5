a506fa3c78902cf10391e5bdedb848aa3748c5c7
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-09-05 17:09:02 -0300

    javascript

--- 09_04/js/abreviaHora.js
+++ 09_04/js/abreviaHora.js
@@ -3,7 +3,7 @@
 function abreviaHora(dataHora){
     const data = new Date(dataHora);
     const hora = data.toLocaleTimeString('pt-br',{hour:'2-digit',minute:'2-digit'});
-    return data;
+    return hora;
 }
 
 export default abreviaHora;
